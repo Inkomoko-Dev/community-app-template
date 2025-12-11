f37a9ad244121453249b448399d8599ad4fc07c2
Niceta Nduku <niceta@inkomoko.com>
2025-12-11 12:39:30 +0300

    CGLT-475: validate note error fixed

--- app/scripts/controllers/loanAccount/LoanAccountActionsController.js
+++ app/scripts/controllers/loanAccount/LoanAccountActionsController.js
@@ -787,8 +787,8 @@
             scope.submit = function () {
                 scope.processDate = false;
                 // Only validate the note field if it is shown and mandatory
-                if (scope.showNoteField && scope.noteFieldMandatory && scope.loanactionform.note.$invalid) {
-                    scope.loanactionform.note.$setTouched();
+                if (scope.showNoteField && scope.noteFieldMandatory && scope.formData.note.$invalid) {
+                    scope.formData.note.$setTouched();
                     window.alert('Note field is mandatory');
                     return; // Prevent submission if note is invalid
                 }
@@ -1255,8 +1255,8 @@
 
             scope.handleReject = function () {
                 // Only validate the note field if it is shown and mandatory
-                if (scope.showNoteField && scope.noteFieldMandatory && scope.loanactionform.note.$invalid) {
-                    scope.loanactionform.note.$setTouched();
+                if (scope.showNoteField && scope.noteFieldMandatory && scope.formData.note.$invalid) {
+                    scope.formData.note.$setTouched();
                     alert("Note field is mandatory");
                     return; // Prevent submission if note is invalid
                 }
