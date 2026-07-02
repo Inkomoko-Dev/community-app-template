56f42409eb19a57720bf9470042a49910777999b
Omoi Nelson <nnyachoti@inkomoko.com>
2026-07-02 11:41:30 +0300

    CGLT-582 add mfi code

--- app/scripts/controllers/loanAccount/LoanAccountActionsController.js
+++ app/scripts/controllers/loanAccount/LoanAccountActionsController.js
@@ -699,6 +699,7 @@
                             
                             // Apply saved disbursement details first
                             if (savedDetail) {
+                                scope.formData.mfiCode = savedDetail.mfiCode || templateData.mfiCode || '';
                                 scope.formData.paymentTo = savedDetail.paymentTo || (templateData.paymentTo ? Number(templateData.paymentTo) : 1);
                                 scope.formData.clientPhoneNumber = savedDetail.clientPhoneNumber || templateData.clientPhoneNumber || '';
                                 scope.formData.clientAccountNumber = savedDetail.clientAccountNumber || templateData.clientAccountNumber || '';
@@ -711,6 +712,7 @@
                                 scope.formData.fxTimestamp = savedDetail.fxTimestamp || templateData.fxTimestamp || null;
                                 scope.formData.usdAmount = savedDetail.usdAmount || templateData.usdAmount || null;
                             } else {
+                                scope.formData.mfiCode = templateData.mfiCode || '';
                                 scope.formData.paymentTo = templateData.paymentTo ? Number(templateData.paymentTo) : 1;
                                 scope.formData.clientPhoneNumber = templateData.clientPhoneNumber || '';
                                 scope.formData.clientAccountNumber = templateData.clientAccountNumber || '';
@@ -1791,6 +1793,7 @@
                             delete submitData.usdAmount;
                             delete submitData.fxSource;
                             delete submitData.fxTimestamp;
+                            delete submitData.mfiCode;
                         }
 
                         if (scope.action === "undoapproval" || scope.action === "undodisbursal") {
