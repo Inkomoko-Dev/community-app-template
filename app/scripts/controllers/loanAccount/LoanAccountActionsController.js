584e7514065791d7fb7128d0b6e1078fb554c8a5
wmnjugunaink <wilfredn@inkomoko.com>
2026-08-13 11:50:33 +0300

    CGLT-582: Make the MFI code editable at the disbursement request stage.

--- app/scripts/controllers/loanAccount/LoanAccountActionsController.js
+++ app/scripts/controllers/loanAccount/LoanAccountActionsController.js
@@ -2158,6 +2158,11 @@
                 return !(isDisbursementApprovalAction && (scope.isCashPayment() || scope.enableThirdPartyDisbursement));
             };
 
+            scope.isMfiCodeReadOnly = function () {
+                const isApprovalOrDRStage = scope.action === "approve" || scope.action === "disbursementpreapprovalrequest";
+                return !isApprovalOrDRStage;
+            };
+
             scope.isCashPayment = function () {
                 if (!Array.isArray(scope.paymentTypes)) return false;
 
