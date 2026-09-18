d9fe08cd252f8da243f4b45d924ba95103af0dbd
Felix Mutua <felixm@inkomoko.com>
2026-09-18 10:46:38 +0300

    CGLT-618: Remove date range from classification summary.
    Summary is a current snapshot, so date pickers no longer match the API.

--- app/scripts/controllers/organization/loanclassification/LoanClassificationSummaryController.js
+++ app/scripts/controllers/organization/loanclassification/LoanClassificationSummaryController.js
@@ -1,15 +1,11 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        LoanClassificationSummaryController: function (scope, resourceFactory, dateFilter) {
+        LoanClassificationSummaryController: function (scope, resourceFactory) {
             scope.offices = [];
             scope.products = [];
             scope.countries = [];
             scope.rows = [];
             scope.formData = {};
-            scope.first = {};
-            scope.first.fromDate = new Date();
-            scope.first.fromDate.setFullYear(scope.first.fromDate.getFullYear() - 1);
-            scope.first.toDate = new Date();
 
             resourceFactory.officeResource.getAllOffices(function (data) {
                 scope.offices = data;
@@ -34,12 +30,6 @@
                 if (scope.formData.loanProductId) {
                     params.loanProductId = scope.formData.loanProductId;
                 }
-                if (scope.first.fromDate) {
-                    params.fromDate = dateFilter(scope.first.fromDate, 'yyyy-MM-dd');
-                }
-                if (scope.first.toDate) {
-                    params.toDate = dateFilter(scope.first.toDate, 'yyyy-MM-dd');
-                }
                 resourceFactory.loanClassificationSummaryResource.get(params, function (data) {
                     scope.rows = data;
                 });
@@ -48,7 +38,7 @@
             scope.search();
         }
     });
-    mifosX.ng.application.controller('LoanClassificationSummaryController', ['$scope', 'ResourceFactory', 'dateFilter', mifosX.controllers.LoanClassificationSummaryController]).run(function ($log) {
+    mifosX.ng.application.controller('LoanClassificationSummaryController', ['$scope', 'ResourceFactory', mifosX.controllers.LoanClassificationSummaryController]).run(function ($log) {
         $log.info('LoanClassificationSummaryController initialized');
     });
 }(mifosX.controllers || {}));
